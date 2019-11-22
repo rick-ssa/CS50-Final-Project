2e8f5546099f75619664fda6fe20debcf40d23fe
Ricardo <rick_ssa@hotmail.com>
2019-11-22 00:48:10 -0300

    conecting with the api

--- frontend/src/js/initializeMap.js
+++ frontend/src/js/initializeMap.js
@@ -1,7 +1,19 @@
-const axios = require('axios');
+
+
+const DAY_MILLISEC = 86400000;
+var eventDate = document.getElementById('dateEvent');
+var todayMilli =  new Date().getTime();
+var dayStartMilli = todayMilli - (30 * DAY_MILLISEC)
+var locations=[]
+
+axios.get(`http://192.168.0.101:3333/local?dateStart=${dayStartMilli}&dateEnd=${todayMilli}`)
+.then(response=>{
+    locations = response.data;
+    console.log(response.data)
+})
 
 function initMap() {
-
+    
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 10,
       center: {lat: -12.983010073106819, lng: -38.48730705029209}
@@ -14,18 +26,35 @@ function initMap() {
                 size: new google.maps.Size(32,32), 
                 scaledSize: new google.maps.Size(32,32),
             }
-            console.log(image)
+            
             if(!bts[0].classList.contains('disabled')){
-                markerCluster.markers_.push(
-                    new google.maps.Marker(
-                        {
-                            position:{lat:e.latLng.lat(), lng: e.latLng.lng()},
-                            animation: google.maps.Animation.DROP,
-                            icon: image,
-                        }
-                    )
+                var dt = new Date(eventDate.value)
+                dt = dt.getFullYear() + '-' + (dt.getMonth()+1) + '-' + dt.getDate()
+                var contentString = `<div id='content'><div id='ev'>${urlIcon.replace('../assets/','').replace('png','')}</div><div id='dt'>${dt}</div></div>`
+                var infoWindow = new google.maps.InfoWindow({content:contentString})
+                var mkr = new google.maps.Marker(
+                    {
+                        position:{lat:e.latLng.lat(), lng: e.latLng.lng()},
+                        animation: google.maps.Animation.DROP,
+                        icon: image,
+                    }
                 )
+                mkr.addListener('click',()=>infoWindow.open(map,mkr))
+                markerCluster.markers_.push(
+                    mkr
+                )
+                
+                
+                
+
                 map.setCenter({lat:map.getCenter().lat() + 0.00001, lng:map.getCenter().lng()})
+                axios.post('http://192.168.0.101:3333/local',{
+                    lat:e.latLng.lat(),
+                    lng:e.latLng.lng(),
+                    dateEvent:new Date(eventDate.value).getTime(),
+                    event:urlIcon.replace('../assets/','').replace('.png',''),
+                })
+                .then(response=>console.log(response))
             }
         }
     )
@@ -35,64 +64,77 @@ function initMap() {
     // Note: The code uses the JavaScript Array.prototype.map() method to
     // create an array of markers based on a given "locations" array.
     // The map() method here has nothing to do with the Google Maps API.
+    
     var markers = locations.map(function(location, i) {
-      return new google.maps.Marker({
-        position: location,
-      });
+        var image={
+            url:`../assets/${location.event}.png`, 
+            size: new google.maps.Size(32,32), 
+            scaledSize: new google.maps.Size(32,32),
+        }
+        var dt = new Date(location.dateEvent)
+        dt = dt.getFullYear() + '-' + (dt.getMonth()+1) + '-' + dt.getDate()
+        var contentString = `<div id='content'><div id='ev'>${location.event}</div><div id='dt'>${dt}</div></div>`
+        var infoWindow = new google.maps.InfoWindow({content:contentString})
+        var mkr = new google.maps.Marker({
+            position: {lat:location.lat,lng:location.lng},
+            animation: google.maps.Animation.DROP,
+            icon: image,
+        });
+        mkr.addListener('click',()=>infoWindow.open(map,mkr))
+        return mkr
     });
-
+    
     // Add a marker clusterer to manage the markers.
     var markerCluster = new MarkerClusterer(map, markers,
         {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 
 }
-  var locations = []
 
-  var urlIcon;
+var urlIcon;
 
-  let bts = document.getElementsByClassName('event')
+let bts = document.getElementsByClassName('event')
 
-  for(let i=0; i<4; i++) {
-      bts[i].addEventListener("click",()=>{
-          if(bts[i].classList.contains('enabled')){
-            urlIcon = `../assets/${bts[i].id}.png`
-            desactiveAllBts()
-            bts[i].classList.add('event-active')
-            enabled(true)
+for(let i=0; i<4; i++) {
+    bts[i].addEventListener("click",()=>{
+        if(bts[i].classList.contains('enabled')){
+        urlIcon = `../assets/${bts[i].id}.png`
+        desactiveAllBts()
+        bts[i].classList.add('event-active')
+        enabled(true)
+        bts[i].classList.remove('enabled')
+        }
+    })
+}
+
+function desactiveAllBts(){
+for(let i=0; i<4; i++) {
+    if (bts[i].classList.contains('event-active')) {
+        bts[i].classList.remove('event-active')
+    }
+}
+}
+
+function enabled(isEnabled) {
+    for(let i=0; i<bts.length; i++){
+        if(isEnabled) {
+        if (bts[i].classList.contains('disabled')) {
+            bts[i].classList.remove('disabled')
+        }
+        bts[i].classList.add('enabled')
+        } else {
+        if (bts[i].classList.contains('enabled')) {
             bts[i].classList.remove('enabled')
-          }
-      })
-  }
-
-  function desactiveAllBts(){
-    for(let i=0; i<4; i++) {
-        if (bts[i].classList.contains('event-active')) {
-            bts[i].classList.remove('event-active')
+        }
+        bts[i].classList.add('disabled')
         }
     }
-  }
+}
 
-  function enabled(isEnabled) {
-      for(let i=0; i<bts.length; i++){
-          if(isEnabled) {
-            if (bts[i].classList.contains('disabled')) {
-                bts[i].classList.remove('disabled')
-            }
-            bts[i].classList.add('enabled')
-          } else {
-            if (bts[i].classList.contains('enabled')) {
-                bts[i].classList.remove('enabled')
-            }
-            bts[i].classList.add('disabled')
-          }
-      }
-  }
 
-  var eventDate = document.getElementById('dateEvent');
 
-  eventDate.addEventListener("change",(e)=>{
-      enabled(true);
-  })
+eventDate.addEventListener("change",(e)=>{
+    enabled(true);
+})
 
 
 
